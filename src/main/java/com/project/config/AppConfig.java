015b5f5af70da9e0ea25c223bdd3fab1f90e3b3d
miniscv <ahdzl126@naver.com>
2025-09-22 15:55:34 +0900

    RestTemplate builder로 변경

--- src/main/java/com/project/config/AppConfig.java
+++ src/main/java/com/project/config/AppConfig.java
@@ -1,23 +1,17 @@
 package com.project.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 public class AppConfig {
 	@Bean
-	RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
-	
-    @Bean
-    ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        return objectMapper;
+	RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.requestFactory(() -> new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()))
+                .build();
     }
 }
