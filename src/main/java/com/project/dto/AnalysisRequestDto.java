27d18adcaab48390c6dd64af9c245357c5377678
miniscv <ahdzl126@naver.com>
2025-09-23 09:50:45 +0900

    python 응답 구조 변경

--- src/main/java/com/project/dto/AnalysisRequestDto.java
+++ src/main/java/com/project/dto/AnalysisRequestDto.java
@@ -2,6 +2,6 @@ package com.project.dto;
 
 import java.util.List;
 
-public record AnalysisRequestDto(String result, String validationMsg, OverallResultDto overallResult,
+public record AnalysisRequestDto(OverallResultDto overallResult,
 		List<DialogueDto> dialogueResult) {
 }
